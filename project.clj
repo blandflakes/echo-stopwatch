9b17f3860bb5edcda8badf4310d7e756cee016b1
blandflakes <xclite@gmail.com>
2015-12-05 17:54:25 -0500

    Trying new version of middleware which packages a local repo differently.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [echo-chamber "0.1.0"]
-                 [echo-chamber-middleware "0.1.0"]
+                 [echo-chamber-middleware "0.1.1"]
                  [ring/ring-core "1.4.0"]
                  [ring/ring-defaults "0.1.2"]
                  [ring/ring-jetty-adapter "1.4.0"]
