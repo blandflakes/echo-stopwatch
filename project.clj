f0261ec5bd2aff603aa011b9833fdf4719824181
blandflakes <xclite@gmail.com>
2015-12-05 19:41:38 -0500

    Updating echo-chamber version for timestamp schema fix.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/blandflakes/echo-stopwatch"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [echo-chamber "0.1.0"]
+                 [echo-chamber "0.1.1"]
                  [echo-chamber-middleware "0.1.3-SNAPSHOT"]
                  [ring/ring-core "1.4.0"]
                  [ring/ring-defaults "0.1.2"]
